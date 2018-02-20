7e7b37740e31cac85b24bd0c6694933f2ae22b9d
Takao Sekiguchi <takao.sekiguchi@soliton.co.jp>
2018-02-20 19:30:52 +0900

    最小二乗回帰

---
+++ least-square/least_square.py
@@ -0,0 +1,57 @@
+# coding: UTF-8
+import numpy as np
+import matplotlib.pyplot as plt
+
+# テストデータ生成
+class GenModel:
+    # ノイズ無し
+    def generate(self, x):
+        a = 4.0
+        b = 10.0
+        y = a * x + b
+        return y
+
+    # ノイズつき
+    def observe(self, x):
+        y = self.generate(x) + np.random.normal(0, 1.0, x.shape)
+        return y
+
+# 最小2乗回帰
+class LeastSquare:
+    # データを線形基底関数による表示に直す
+    def __data_vec(self, x):
+        return np.array([1.0, x])
+
+    # 計画行列 X を返す
+    def __design_mat(self, xv):
+        return np.array([self.__data_vec(x) for x in xv])
+
+    # 学習データを入力して係数wを決定する
+    def fit(self, xv, y):
+        X = self.__design_mat(x)
+        
+        # solve (X^t X) w = X^t y        
+        self.w = np.linalg.solve(np.dot(X.T, X), np.dot(X.T, y))
+        print('w', self.w)
+        return
+    
+    # 予測値を返す
+    #    xv は1次元配列前提, 1点のxに対しても同じように計算できるようにしたい...
+    def predict(self, xv):
+        # y_pred = X w
+        return np.dot(self.__design_mat(xv), self.w)
+
+if __name__ == '__main__':
+    gen_model = GenModel()
+    x = 0.1 * np.arange(100)
+    y = gen_model.observe(x)
+    # t = gen_model.generate(x)
+
+    lls = LeastSquare()
+    lls.fit(x, y)
+    y_pred = lls.predict(x)
+
+    plt.scatter(x, y, color='lightblue')
+    plt.plot(x, y_pred, color='red')
+    # plt.plot(x, t, color='red')
+    plt.show()
