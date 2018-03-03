aa9a7b82278f4ef1c5f0184bda6c028c139c94c0
Takao Sekiguchi <takao1080@gmail.com>
2018-03-04 01:29:20 +0900

    refactor

--- least-square/kernel_regression.py
+++ least-square/kernel_regression.py
@@ -18,41 +18,42 @@ class GenModel:
 
 # ガウスカーネル回帰
 class KernelRegression:
-    # beta
+    # beta: ガウスカーネルパラメータ
+    # lam : 正則化係数
     def __init__(self, beta, lam):
         self.beta = beta
         self.lam = lam
         
-    # データを多項式基底関数による表示に直す
+    # gauss kernel
     def __kernel(self, x1, x2):
         return np.exp(-self.beta * (x1 - x2)**2)
 
-    # グラム行列 K を返す
+    # グラム行列を返す
     def __gram_mat(self, x):
         N = x.size
         K = np.empty((N, N))
-        for i in np.arange(N):
-            for j in np.arange(N):
+        for i in range(N):
+            for j in range(N):
                 K[i, j] = self.__kernel(x[i], x[j])
         return K
 
-    # 学習データを入力して係数wを決定する
+    # 学習データを入力して係数aを決定する
     def fit(self, x, y):
-        self.N = x.size
+        # 学習データを保持する
         self.x = x
+
+        # solve (K + lambda I) a = y
         K = self.__gram_mat(x)
-        
-        # solve (K + lambda I) a = y        
-        self.a = np.linalg.solve(K + self.lam * np.eye(self.N), y)
+        self.a = np.linalg.solve(K + self.lam * np.eye(x.size), y)
         
     # 予測値を返す
     def predict(self, x):
-        y = np.empty(x.shape)
-        for i in np.arange(x.size):
-            y[i] = 0
-            for j in np.arange(self.N):
-                y[i] += self.__kernel(x[i], self.x[j]) * self.a[j]
-        return y
+        # \sum_j k(x, k_j) a_j
+        K = np.empty((x.size, self.x.size))
+        for i in range(x.size):
+            for j in range(self.x.size):
+                K[i, j] = self.__kernel(x[i], self.x[j])
+        return K.dot(self.a)
     
 if __name__ == '__main__':
     gen_model = GenModel()
@@ -60,7 +61,7 @@ if __name__ == '__main__':
     y = gen_model.observe(x)
     t = gen_model.generate(x)
 
-    kr = KernelRegression(1.0, 0.1)
+    kr = KernelRegression(beta=1.0, lam=0.1)
     kr.fit(x, y)
     print('a', kr.a)
     
@@ -70,4 +71,4 @@ if __name__ == '__main__':
     plt.plot(x, t, color='blue')
     
     plt.show()
-    # plt.savefig('kernel_regression_poly.png')
+    # plt.savefig('kernel_regression.png')
