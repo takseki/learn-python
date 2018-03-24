aba616a74af83547dab945f86cd364489bb13809
Takao Sekiguchi <takao1080@gmail.com>
2018-03-24 23:56:59 +0900

    refactor

--- pca/pca.py
+++ pca/pca.py
@@ -1,16 +1,18 @@
 # coding: UTF-8
 import numpy as np
 import matplotlib.pyplot as plt
+import operator as op
 
 # テストデータ生成
 class GenModel:
-    # ノイズ無し
+    # 2次元正規分布
     def generate(self, size):
         mean = np.zeros(2)
         c = np.cos(np.pi/6)
         s = np.sin(np.pi/6)
         R = np.array([[c, -s], [s,c]])
-        cov = R.dot(np.diag([10.0, 1.0])).dot(R.T)
+        # 回転させる
+        cov = R.dot(np.diag([1.0, 0.2])).dot(R.T)
         y = np.random.multivariate_normal(mean, cov, size)
         return y
 
@@ -21,33 +23,44 @@ class Pca:
     def fit(self, X):
         # 共分散行列
         Sigma = np.dot(X.T, X)
-        self.l, self.v = np.linalg.eigh(Sigma)
-        indicies = np.argsort(self.l)
-        self.l = [self.l[i] for i in indicies[::-1]]
-        self.v = np.array([self.v[i] for i in indicies[::-1]])
-        print('eigen values',  self.l)
-        print('eigen vectors', self.v)
+        vals, vecs = np.linalg.eigh(Sigma)
+        # 固有値の大きい順にソート
+        pairs = list(zip(vals, vecs))
+        pairs.sort(key=op.itemgetter(0), reverse=True)
+        vals, vecs = zip(*pairs)
+        # 固有ベクトルを保持
+        self.v = np.array(vecs)
 
-    # 主成分を返す
-    def transform(self, x):
-        return np.dot(x, self.v.T)
+    # x を主成分表示にして返す
+    # n: 抽出する主成分の数
+    def transform(self, x, n):
+        return np.dot(x, self.v[:n].T)
     
 if __name__ == '__main__':
     gen_model = GenModel()
     x = gen_model.generate(500)
     x = x - np.mean(x, axis=0)
 
-    fig, (ax1, ax2) = plt.subplots(ncols=2, figsize=(10,4))
-    
-    ax1.scatter(x[:,0], x[:, 1], marker=".")
-    ax1.axis([-10, 10, -10, 10])
-    ax1.grid(True)
-    
     pca = Pca()
     pca.fit(x)
-    y = pca.transform(x)
+    y = pca.transform(x, 2)
+    
+    fig, axes = plt.subplots(ncols=2, figsize=(8,4))
 
+    for ax in axes:
+        r = 4
+        ax.axis([-r, r, -r, r])
+        ax.grid(True)
+        ax.set_aspect('equal')
+
+    (ax1, ax2) = axes
+    # 元データ
+    ax1.scatter(x[:,0], x[:, 1], marker=".")
+    # 固有ベクトルを描く
+    num = pca.v.shape[0]
+    ax1.quiver(np.zeros(num), np.zeros(num),
+               4 * pca.v[:, 0], 4 * pca.v[:, 1], angles='xy', scale_units='xy', scale=1)
+
+    # 主成分
     ax2.scatter(y[:,0], y[:, 1], marker=".")
-    ax2.axis([-10, 10, -10, 10])
-    ax2.grid(True)
     plt.show()
