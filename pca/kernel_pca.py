13d6aef166ab1b1012bb645c974a2c1ff3fa79ee
Takao Sekiguchi <takao1080@gmail.com>
2018-03-25 16:37:35 +0900

    主成分の特徴量平面上へのプロットを追加

--- pca/kernel_pca.py
+++ pca/kernel_pca.py
@@ -3,19 +3,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.decomposition import KernelPCA
 from sklearn.preprocessing import KernelCenterer
+from matplotlib.colors import ListedColormap
 
 # テストデータ生成
 class GenModel:
     # 2次元正規分布
     def generate(self, size):
-        mean = np.zeros(2)
-        c = np.cos(np.pi/6)
-        s = np.sin(np.pi/6)
-        R = np.array([[c, -s], [s,c]])
-        # 回転させる
-        cov = R.dot(np.diag([1.0, 0.2])).dot(R.T)
-        y = np.random.multivariate_normal(mean, cov, size)
-        return y
+        mean = np.array([-2., -2.])
+        cov = np.diag([0.2, 0.2])
+        g_size = size // 3
+        y1 = np.random.multivariate_normal(mean, cov, g_size)
+        mean = np.array([2., -1.5])
+        y2 = np.random.multivariate_normal(mean, cov, g_size)
+        mean = np.array([0, 1.5])
+        y3 = np.random.multivariate_normal(mean, cov, size - g_size*2)
+        return np.row_stack((y1, y2, y3))
 
 # カーネル主成分分析
 class KernelPca:
@@ -36,7 +38,7 @@ class KernelPca:
         # グラム行列
         N = X.shape[0]
         K = np.array(
-            [[self.__kernel(X[i], X[j]) for i in range(N)] for j in range(N)])
+            [[self.__kernel(X[i], X[j]) for j in range(N)] for i in range(N)])
         # 中心化
         K = self.centerer.fit_transform(K)
         # eighは固有値の昇順で出力される
@@ -55,7 +57,7 @@ class KernelPca:
         N = self.X.shape[0]
         Nx = x.shape[0]
         K = np.array(
-            [[self.__kernel(x[i], self.X[j]) for i in range(Nx)] for j in range(N)]
+            [[self.__kernel(x[i], self.X[j]) for j in range(N)] for i in range(Nx)]
         )                        # (Nx,N)
         # 中心化
         K = self.centerer.transform(K)
@@ -64,17 +66,17 @@ class KernelPca:
     
 if __name__ == '__main__':
     gen_model = GenModel()
-    x = gen_model.generate(100)
-    x = x - np.mean(x, axis=0)
+    X = gen_model.generate(60)
+    X = X - np.mean(X, axis=0)
 
-    beta = 0.1
+    beta = 0.5
     pca = KernelPca(beta)
-    y = pca.fit_transform(x, 2)
-    y2 = pca.transform(x)
+    y = pca.fit_transform(X, 2)
+    y2 = pca.transform(X)
 
     scikit_kpca = KernelPCA(n_components=2, kernel='rbf', gamma=beta)
-    y1 = scikit_kpca.fit_transform(x)
-    y_kpca = scikit_kpca.transform(x)
+    y1 = scikit_kpca.fit_transform(X)
+    y_kpca = scikit_kpca.transform(X)
 
     print('        transform    ', y2[0])
     print('        fit_transform', y[0])
@@ -90,20 +92,34 @@ if __name__ == '__main__':
         ax.set_aspect('equal')
 
     (ax1, ax2, ax3) = axes
-    r = 1
-    ax2.axis([-r, r, -r, r])
-    ax3.axis([-r, r, -r, r])
-    # 元データ
-    ax1.scatter(x[:,0], x[:, 1], marker=".")
-    # 固有ベクトルを描く
-    # num = pca.v.shape[0]
-    # ax1.quiver(np.zeros(num), np.zeros(num),
-    # 4 * pca.v[:, 0], 4 * pca.v[:, 1], angles='xy', scale_units='xy', scale=1)
+
 
     # 主成分
-    ax2.scatter(y[:,0], y[:, 1], marker=".")
-    ax3.scatter(y_kpca[:,0], y_kpca[:, 1], marker=".")
+    ax.axis([-1, 1, -1, 1])
+    ax3.scatter(y[:,0], y[:, 1], marker=".")
+
+    # PRML 12章にあるような元の空間での基底関数のプロットをしてみる
+    resolution=0.1
+    xx1, xx2 = np.meshgrid(np.arange(-4, 4, resolution),
+                           np.arange(-4, 4, resolution))
+    mesh = np.array([xx1.ravel(), xx2.ravel()]).T
     
+    z = pca.transform(mesh)
+    
+    # デフォルトのcontour色だとデータ点と重なった時見づらい
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:1])
+    
+    # 上位成分を元空間上に等高線図でプロット
+    for i in range(2):
+        ax = axes[i]
+        #ax.scatter(X[:, 0], X[:, 1], color='red', marker='^', alpha=0.5)
+        z_i = z[:, i].reshape(xx1.shape)
+        ax.pcolor(xx1, xx2, z_i, cmap='RdBu')
+
+    ax1.scatter(X[:,0], X[:, 1], marker=".", color='black')
+    ax2.scatter(X[:,0], X[:, 1], marker=".", color='black')
+
     plt.show()
-    # plt.savefig('kernel_pca.png')
+    #plt.savefig('kernel_pca.png')
     
