70e9070986249a3b2e5572f3d107b64f99b1b2df
Takao Sekiguchi <takao1080@gmail.com>
2018-03-25 14:56:31 +0900

    transform を直した
    scikit-learn のソースを見ると、どうも中心化が必要らしい
    scikit-learn の KernelCenterer を使って中心化した

--- pca/kernel_pca.py
+++ pca/kernel_pca.py
@@ -1,9 +1,8 @@
 # coding: UTF-8
 import numpy as np
 import matplotlib.pyplot as plt
-import operator as op
-#import sklearn as sk
 from sklearn.decomposition import KernelPCA
+from sklearn.preprocessing import KernelCenterer
 
 # テストデータ生成
 class GenModel:
@@ -23,62 +22,65 @@ class KernelPca:
     # beta: ガウスカーネルパラメータ
     def __init__(self, beta):
         self.beta = beta
+        self.centerer = KernelCenterer()
 
     # gauss kernel
     def __kernel(self, x1, x2):
         return np.exp(-self.beta * np.linalg.norm(x1 - x2)**2)
 
-    # 中心化したグラム行列を返す
-    def __gram_mat(self, X):
-        N = X.shape[0]
-        K = np.array(
-            [[self.__kernel(X[i], X[j]) for i in range(N)] for j in range(N)])
-        one = 1/N * np.ones(K.shape)
-        return K - one.dot(K) - K.dot(one) + one.dot(K).dot(one)
-        #K = np.fromfunction(lambda i, j:
-        #                    self.__kernel(X[i], X[j]), (N, N), dtype=int)
-    
     # データを入力して主成分ベクトルを計算する
+    # shape(X) = (N, M)
     # n: 抽出する主成分の数
     def fit_transform(self, X, n):
         self.X = X
         # グラム行列
-        K = self.__gram_mat(X)
-        vals, vecs = np.linalg.eigh(K)
+        N = X.shape[0]
+        K = np.array(
+            [[self.__kernel(X[i], X[j]) for i in range(N)] for j in range(N)])
+        # 中心化
+        K = self.centerer.fit_transform(K)
         # eighは固有値の昇順で出力される
+        vals, vecs = np.linalg.eigh(K)
         vals = vals[::-1]
         vecs = vecs[:, ::-1]
-        # 特異値と左特異ベクトル
-        self.sigma = np.sqrt(vals[:n]) # (n)
-        self.a = np.array(vecs[:, :n]).T  # (n,N)
-        return self.a.T * self.sigma   # (N,n)
+        # 特異値と左特異ベクトル、上位n個
+        self.sigma = np.sqrt(vals[:n])  # (n)
+        self.a = np.array(vecs[:, :n])  # (N,n)
+        return self.sigma * self.a      # (N,n)
 
     # xの主成分表示を返す
+    # shape(x)=(Nx, M)
     def transform(self, x):
+        # グラム行列
         N = self.X.shape[0]
-        n = self.sigma.size
-        normalized_a = self.a.T / self.sigma # (N,n)
-        y = np.zeros(n)
-        k = np.array([self.__kernel(x, self.X[i]) for i in range(N)]) #(N)
-        y = k.dot(normalized_a) # (n)
-        return y
+        Nx = x.shape[0]
+        K = np.array(
+            [[self.__kernel(x[i], self.X[j]) for i in range(Nx)] for j in range(N)]
+        )                        # (Nx,N)
+        # 中心化
+        K = self.centerer.transform(K)
+        # 主成分を計算
+        return K.dot(self.a) / self.sigma  # (Nx,n)
     
 if __name__ == '__main__':
     gen_model = GenModel()
     x = gen_model.generate(100)
     x = x - np.mean(x, axis=0)
 
-    beta=1.0
+    beta = 0.1
     pca = KernelPca(beta)
     y = pca.fit_transform(x, 2)
+    y2 = pca.transform(x)
 
     scikit_kpca = KernelPCA(n_components=2, kernel='rbf', gamma=beta)
-    y_kpca = scikit_kpca.fit_transform(x)
+    y1 = scikit_kpca.fit_transform(x)
+    y_kpca = scikit_kpca.transform(x)
 
-    print(pca.transform(x[0]))
-    print(scikit_kpca.transform([x[0]]))
-    print(y[0])
-    
+    print('        transform    ', y2[0])
+    print('        fit_transform', y[0])
+    print('sklearn transform    ', y_kpca[0])
+    print('sklearn fit_transform', y1[0])
+
     fig, axes = plt.subplots(ncols=3, figsize=(12,4))
 
     for ax in axes:
@@ -88,6 +90,9 @@ if __name__ == '__main__':
         ax.set_aspect('equal')
 
     (ax1, ax2, ax3) = axes
+    r = 1
+    ax2.axis([-r, r, -r, r])
+    ax3.axis([-r, r, -r, r])
     # 元データ
     ax1.scatter(x[:,0], x[:, 1], marker=".")
     # 固有ベクトルを描く
